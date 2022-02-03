c21d5fd8a95fc53eb87a0d3dbbf9f899d6743a9e
rabie85 <dif.rabie@gmail.com>
2022-02-03 17:22:13 +0100

    add methods for response api

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -10,4 +10,18 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function responseValidator($errors)
+    {
+        return response()->json(['success'=>false,'type'=>'validator','data' => $errors]);
+    }
+
+    public function responseMessage($message)
+    {
+        return response()->json(['success'=>false,'type'=>'message','data' => $message]);
+    }
+    public function responseData($data)
+    {
+        return response()->json(['success'=>true,'data' => $data]);
+    }
 }
